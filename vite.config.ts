33078ec71f7c03bafb6cf825e236286ff453343d
Zachary-Higgins <zachhiggins+1@gmail.com>
2026-01-17 02:20:49 -0500

    refactor: move build output to build/ folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   publicDir: 'content',
+  build: {
+    outDir: 'build',
+  },
   server: {
     port: 5173,
     strictPort: false,
